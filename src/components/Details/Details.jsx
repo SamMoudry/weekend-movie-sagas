e05ba22686e4c27c30504f8e114f9fab9a06f234
SamMoudry <sammoudry@gmail.com>
2021-05-15 21:13:17 -0500

    struggling to get stuff to show up right on Details.jsx

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,16 +1,46 @@
 import {useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 
 function Details() {
     const history = useHistory();
+    const [title, setTitle] = useState('');
+    const [poster, setPoster] = useState('');
+    const [description, setDescription] = useState('');
+    const [genre, setGenre] = useState([]);
     const movieDetails = useSelector(store => store.details);
+
+    const map = () => {
+        movieDetails.map(item => {
+            setTitle(item.title);
+            setPoster(item.poster);
+            setDescription(item.description);
+            setGenre(genre.concat(item.name));
+        })
+    }
+
+
+    useEffect(() => {
+        map();
+    }, []);
 
     const handleSubmit = () => {
         history.push('/');
     }
+
     return (
         <>
-        <div>Movie Details here.</div>
+        <div>
+        { movieDetails ? 
+                (
+                    <div>
+                        <div>{title}</div>
+                        <img src={poster} alt={title} />
+                        <div>{description}</div>
+                    </div>
+                ) : ''
+            }
+        </div>
         <form onSubmit={handleSubmit}>
             <button>Back to List</button>
         </form>
